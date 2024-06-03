2254aca60696b1390b91bf6d4dadec659133c7ad
YevhenKharchenko <evgenvalerych@gmail.com>
2024-06-03 11:35:40 +0300

    validation schema updated

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,26 +2,55 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required().messages({
-    'string.base': 'Usernameshould be a string',
+    'string.base': 'Username should be a string',
     'string.min': 'Username have at least {#limit} characters',
     'string.max': 'Username have at most {#limit} characters',
     'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().required(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  phoneNumber: Joi.string().min(3).max(20).required().messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number have at least {#limit} characters',
+    'string.max': 'Phone number have at most {#limit} characters',
+    'any.required': 'Phone number is required',
+  }),
+  email: Joi.string().email().required().messages({
+    'string.base': 'Email should be a string',
+    'any.required': 'Email is required',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'isFavourite should be a boolean',
+  }),
+  contactType: Joi.string()
+    .valid('work', 'home', 'personal')
+    .required()
+    .messages({
+      'string.base': 'Contact type should be a string',
+      'any.only':
+        'Contact type must be one of the following: work, home, personal',
+      'any.required': 'Contact type is required',
+    }),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).messages({
-    'string.base': 'Usernameshould be a string',
+    'string.base': 'Username should be a string',
     'string.min': 'Username have at least {#limit} characters',
     'string.max': 'Username have at most {#limit} characters',
-    'any.required': 'Username is required',
   }),
-  phoneNumber: Joi.string().min(3).max(20),
-  email: Joi.string().email(),
-  isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
+  phoneNumber: Joi.string().min(3).max(20).messages({
+    'string.base': 'Phone number should be a string',
+    'string.min': 'Phone number have at least {#limit} characters',
+    'string.max': 'Phone number have at most {#limit} characters',
+  }),
+  email: Joi.string().email().messages({
+    'string.base': 'Email should be a string',
+  }),
+  isFavourite: Joi.boolean().messages({
+    'boolean.base': 'isFavourite should be a boolean',
+  }),
+  contactType: Joi.string().valid('work', 'home', 'personal').messages({
+    'string.base': 'Contact type should be a string',
+    'any.only':
+      'Contact type must be one of the following: work, home, personal',
+  }),
 });
