1a4be608b77f3c8143cf903d5c5357eff1179cda
YevhenKharchenko <evgenvalerych@gmail.com>
2024-06-25 12:28:43 +0300

    bugfix

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -34,6 +35,8 @@ export const setupServer = () => {
   app.use(router);
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+
+  app.use('/api-docs', swaggerDocs());
 
   app.use('*', notFoundHandler);
 
